17ddf798f276b0e3de8ace87f91593db9c5abfe5
james.sweeney <james.sweeney@liquidweb.com>
2024-02-04 16:33:51 -0600

    add Button

---
+++ src/components/Button.vue
@@ -0,0 +1,64 @@
+<script setup>
+const props = defineProps({
+  variant: String,
+  disabled: Boolean,
+  loading: Boolean,
+});
+</script>
+
+<template>
+  <button :class="variant" :disabled="Boolean(disabled || loading)">
+    <span v-if="loading">loading...</span>
+    <slot v-else></slot>
+  </button>
+</template>
+
+<style>
+button {
+  padding: 10px 18px;
+  font-weight: 500;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: opacity ease-in-out 0.1s;
+}
+
+.tertiary {
+  background-color: transparent;
+  color: var(--cool-gray);
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:focus {
+    opacity: 0.8;
+  }
+}
+
+.secondary {
+  background-color: var(--marine-blue);
+  color: white;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:focus {
+    opacity: 0.8;
+  }
+}
+
+.primary {
+  background-color: var(--purplish-blue);
+  color: white;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:focus {
+    opacity: 0.8;
+  }
+}
+</style>
