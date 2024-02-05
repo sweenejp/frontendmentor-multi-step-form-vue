7f9c13ba3d3c0ed92417939aee1dc2519ef7052b
james.sweeney <james.sweeney@liquidweb.com>
2024-02-05 09:07:25 -0600

    add paper

---
+++ src/components/Paper.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="paper">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.paper {
+  background-color: var(--magnolia);
+  padding: 12px;
+  border-radius: 7px;
+}
+</style>
